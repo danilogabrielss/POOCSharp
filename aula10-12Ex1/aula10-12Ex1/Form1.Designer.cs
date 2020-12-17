9c7c75cbafc3d007b6c0f0d91a368dce2af8c0cc
Danilo Gabriel <59902770+danilogabrielss@users.noreply.github.com>
2020-12-17 11:27:17 -0300

    aulas anteriores

---
+++ aula10-12Ex1/aula10-12Ex1/Form1.Designer.cs
@@ -0,0 +1,385 @@
+﻿namespace aula10_12Ex1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnPessoa = new System.Windows.Forms.Button();
+            this.nomeTextBox = new System.Windows.Forms.TextBox();
+            this.telefoneTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.anoTextBox = new System.Windows.Forms.TextBox();
+            this.localTextBox = new System.Windows.Forms.TextBox();
+            this.btnAmigo = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.profissaoTextBox = new System.Windows.Forms.TextBox();
+            this.lclTrabalhoTextBox = new System.Windows.Forms.TextBox();
+            this.btnColega = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lblNomeResultado = new System.Windows.Forms.Label();
+            this.lblTelefoneResultado = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Telefone";
+            // 
+            // btnPessoa
+            // 
+            this.btnPessoa.Location = new System.Drawing.Point(81, 98);
+            this.btnPessoa.Name = "btnPessoa";
+            this.btnPessoa.Size = new System.Drawing.Size(75, 23);
+            this.btnPessoa.TabIndex = 2;
+            this.btnPessoa.Text = "Enviar";
+            this.btnPessoa.UseVisualStyleBackColor = true;
+            // 
+            // nomeTextBox
+            // 
+            this.nomeTextBox.Location = new System.Drawing.Point(161, 40);
+            this.nomeTextBox.Name = "nomeTextBox";
+            this.nomeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nomeTextBox.TabIndex = 3;
+            // 
+            // telefoneTextBox
+            // 
+            this.telefoneTextBox.Location = new System.Drawing.Point(161, 72);
+            this.telefoneTextBox.Name = "telefoneTextBox";
+            this.telefoneTextBox.Size = new System.Drawing.Size(100, 20);
+            this.telefoneTextBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Clique no botão para enviar";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(57, 320);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(137, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Clique no botão para enviar";
+            // 
+            // anoTextBox
+            // 
+            this.anoTextBox.Location = new System.Drawing.Point(161, 268);
+            this.anoTextBox.Name = "anoTextBox";
+            this.anoTextBox.Size = new System.Drawing.Size(100, 20);
+            this.anoTextBox.TabIndex = 10;
+            // 
+            // localTextBox
+            // 
+            this.localTextBox.Location = new System.Drawing.Point(161, 236);
+            this.localTextBox.Name = "localTextBox";
+            this.localTextBox.Size = new System.Drawing.Size(100, 20);
+            this.localTextBox.TabIndex = 9;
+            // 
+            // btnAmigo
+            // 
+            this.btnAmigo.Location = new System.Drawing.Point(81, 294);
+            this.btnAmigo.Name = "btnAmigo";
+            this.btnAmigo.Size = new System.Drawing.Size(75, 23);
+            this.btnAmigo.TabIndex = 8;
+            this.btnAmigo.Text = "Enviar";
+            this.btnAmigo.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 271);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Ano de conhecimento";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 239);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(118, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Local de conhecimento";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(322, 124);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(137, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Clique no botão para enviar";
+            // 
+            // profissaoTextBox
+            // 
+            this.profissaoTextBox.Location = new System.Drawing.Point(426, 72);
+            this.profissaoTextBox.Name = "profissaoTextBox";
+            this.profissaoTextBox.Size = new System.Drawing.Size(100, 20);
+            this.profissaoTextBox.TabIndex = 16;
+            // 
+            // lclTrabalhoTextBox
+            // 
+            this.lclTrabalhoTextBox.Location = new System.Drawing.Point(426, 40);
+            this.lclTrabalhoTextBox.Name = "lclTrabalhoTextBox";
+            this.lclTrabalhoTextBox.Size = new System.Drawing.Size(100, 20);
+            this.lclTrabalhoTextBox.TabIndex = 15;
+            // 
+            // btnColega
+            // 
+            this.btnColega.Location = new System.Drawing.Point(346, 98);
+            this.btnColega.Name = "btnColega";
+            this.btnColega.Size = new System.Drawing.Size(75, 23);
+            this.btnColega.TabIndex = 14;
+            this.btnColega.Text = "Enviar";
+            this.btnColega.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(292, 75);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(50, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Profissão";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(292, 43);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(89, 13);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Local de trabalho";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(27, 154);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(35, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Nome";
+            // 
+            // lblNomeResultado
+            // 
+            this.lblNomeResultado.AutoSize = true;
+            this.lblNomeResultado.Location = new System.Drawing.Point(159, 154);
+            this.lblNomeResultado.Name = "lblNomeResultado";
+            this.lblNomeResultado.Size = new System.Drawing.Size(31, 13);
+            this.lblNomeResultado.TabIndex = 19;
+            this.lblNomeResultado.Text = "        ";
+            // 
+            // lblTelefoneResultado
+            // 
+            this.lblTelefoneResultado.AutoSize = true;
+            this.lblTelefoneResultado.Location = new System.Drawing.Point(159, 186);
+            this.lblTelefoneResultado.Name = "lblTelefoneResultado";
+            this.lblTelefoneResultado.Size = new System.Drawing.Size(31, 13);
+            this.lblTelefoneResultado.TabIndex = 21;
+            this.lblTelefoneResultado.Text = "        ";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(27, 186);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(49, 13);
+            this.label12.TabIndex = 20;
+            this.label12.Text = "Telefone";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(159, 383);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(31, 13);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "        ";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(27, 383);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(111, 13);
+            this.label13.TabIndex = 24;
+            this.label13.Text = "Ano de conhecimento";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(159, 351);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(31, 13);
+            this.label14.TabIndex = 23;
+            this.label14.Text = "        ";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(27, 351);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(118, 13);
+            this.label15.TabIndex = 22;
+            this.label15.Text = "Local do conhecimento";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(97, 19);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(42, 13);
+            this.label16.TabIndex = 26;
+            this.label16.Text = "Pessoa";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(97, 209);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(36, 13);
+            this.label17.TabIndex = 27;
+            this.label17.Text = "Amigo";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(362, 19);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(40, 13);
+            this.label18.TabIndex = 28;
+            this.label18.Text = "Colega";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 408);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.lblTelefoneResultado);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.lblNomeResultado);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.profissaoTextBox);
+            this.Controls.Add(this.lclTrabalhoTextBox);
+            this.Controls.Add(this.btnColega);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.anoTextBox);
+            this.Controls.Add(this.localTextBox);
+            this.Controls.Add(this.btnAmigo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.telefoneTextBox);
+            this.Controls.Add(this.nomeTextBox);
+            this.Controls.Add(this.btnPessoa);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnPessoa;
+        private System.Windows.Forms.TextBox nomeTextBox;
+        private System.Windows.Forms.TextBox telefoneTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox anoTextBox;
+        private System.Windows.Forms.TextBox localTextBox;
+        private System.Windows.Forms.Button btnAmigo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox profissaoTextBox;
+        private System.Windows.Forms.TextBox lclTrabalhoTextBox;
+        private System.Windows.Forms.Button btnColega;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lblNomeResultado;
+        private System.Windows.Forms.Label lblTelefoneResultado;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+    }
+}
+
---
+++ aula10-12Ex1/aula10-12Ex1/Pessoa.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula10_12Ex1
+{
+    class Pessoa
+    {
+        //Atributos
+        private string nome;
+        private string telefone;
+
+        //Construtor
+        public Pessoa()
+        {
+            name = " ";
+            tel = "(12)3456-7890";
+        }
+        // Propriedades
+        public string name
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        public string tel
+        {
+            get { return telefone; }
+            set { telefone = value; }
+        }
+        public class Amigo : Pessoa
+        {
+            //Atributos
+            private string local;
+            private double anoDeConhecimento;
+
+            //Construtor
+            public Amigo()
+            {
+                lugar = " ";
+                ano = 0;
+            }
+            //Propriedades
+            public string lugar
+            {
+                get { return local; }
+                set { local = value; }
+            }
+            public double ano
+            {
+                get { return anoDeConhecimento; }
+                set { anoDeConhecimento = value; }
+            }
+        }
+        public class Colega : Pessoa
+        {
+            //Atributos
+            private string localDeTrabalho;
+            private string profissao;
+
+            //Consturtor
+            public Colega()
+            {
+                local = "";
+                profissa = "";
+            }
+
+            //Propriedades
+            public string local
+            {
+                get { return localDeTrabalho; }
+                set { localDeTrabalho = value; }
+            }
+            public string profissa
+            {
+                get { return profissao; }
+                set { profissao = value; }
+            }
+        }
+    }
+}
